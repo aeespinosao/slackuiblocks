d3c78e6cf91cde546553c8dbd507ee0e35a012c1
Andres Espinosa <andreseespinosao@gmail.com>
2023-08-07 21:38:51 -0500

    add  blocks elements

--- blocks.py
+++ blocks.py
@@ -9,6 +9,8 @@ class Type:
     FILE = "file"
     HEADER = "header"
     IMAGE = "image"
+    INPUT = "input"
+    SECTION = "section"
 
 
 class Context(BaseModel):
@@ -102,3 +104,90 @@ class Image(BaseModel):
             raise ValueError('block_id should be more than 255 char')
         return value
 
+
+class Input(BaseModel):
+    type: str = Field(Type.INPUT, const=True)
+    label: str
+    element: Text # mising checkbox, radio, select, multiselect, datepicker
+    dispatch_action: bool = False
+    block_id: str = None
+    hint: Text = None
+    optional: bool = False
+
+    @validator('block_id')
+    def block_id_validator(cls, value: str) -> str:
+        if len(value) > 255:
+            raise ValueError('block_id should be more than 255 char')
+        return value
+    
+    @validator('hint')
+    def hint_validator(cls, value: Text) -> Text:
+        if len(value.text) > 2000:
+            raise ValueError('hint should be less than 2000 char')
+        return value
+    
+
+class Section(BaseModel):
+    type: str = Field(Type.SECTION, const=True)
+    text: Text = None
+    block_id: str = None
+    fields: list[Text]
+    accessory = None#need elements objects
+    
+    @validator('text')
+    def text_validator(cls, value: Text, values: dict) -> Text:
+        if len(values.get('fields')) == 0:
+            size = len(value.text)
+            if size > 3000 or size < 1:
+                raise ValueError('text should be between 1 and 3000 char')
+        return value
+    
+    @validator('block_id')
+    def block_id_validator(cls, value: str) -> str:
+        if len(value) > 255:
+            raise ValueError('block_id should be less than 255 char')
+        return value
+    
+    @validator('fields')
+    def fields_validator(cls, value: list[Text], values: dict) -> Text:
+        if values.get('text') is None:
+            size = len(value)
+            if size < 1 or size > 10:
+                raise ValueError('fields should be less than 10 elements')
+            
+            for element in value:
+                if len(element.text) > 2000:
+                    raise ValueError('all elements in fields should be less than than 2000 char')
+                    
+        return value
+        
+    
+class Video(BaseModel):
+    alt_text: str
+    author_name: str = None
+    block_id: str = None
+    description: Text = None
+    provider_icon_url: str = None
+    provider_name: str = None
+    title: Text
+    title_url: str = None
+    thumbnail_url: str
+    video_url: str
+    
+    @validator('author_name')
+    def author_name_validator(cls, value: str) -> str:
+        if len(value) > 50:
+            raise ValueError('author_name should be less than 50 char')
+        return value
+    
+    @validator('block_id')
+    def block_id_validator(cls, value: str) -> str:
+        if len(value) > 255:
+            raise ValueError('block_id should be more than 255 char')
+        return value
+    
+    @validator('title')
+    def title_validator(cls, value: str) -> str:
+        if len(value) > 200:
+            raise ValueError('title should be less than 200 char')
+        return value
