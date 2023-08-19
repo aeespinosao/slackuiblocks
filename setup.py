bc4d831b335b5f9bb8b290e4b8dd0f4c4b43f1e7
Andres Espinosa <andreseespinosao@gmail.com>
2023-08-18 23:31:49 -0500

    V0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="slackuiblocks",
-    version="0.0.1",
+    version="0.0.2",
     author="Andres Espinosa",
     description="Slack UI Blocks is a Python library that makes it easy to create and manage Slack UI blocks. It provides a high-level API that abstracts away the details of the Slack Block Kit JSON format, making it easy to build complex UIs with just a few lines of code.",
     packages=["slackuiblocks"],
