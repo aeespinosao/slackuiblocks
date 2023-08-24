c092c00a783e4f3a327cea831287d10f932111fe
Andres Espinosa <andreseespinosao@gmail.com>
2023-08-24 00:11:24 -0500

    add slack views

--- slackuiblocks/views.py
+++ slackuiblocks/views.py
@@ -1,6 +1,37 @@
-from layout_blocks import Section
-from pydantic import BaseModel
+from layout_blocks import LayoutBlock
+from pydantic import BaseModel, Field
+from .composition_objects import PlainText
+
+
+from enum import StrEnum
+
+
+class Type(StrEnum):
+    MODAL = "modal"
+    HOME = "home"
 
 
 class Blocks(BaseModel):
-    blocks: list[Section] = []
+    blocks: list[LayoutBlock] = []
+
+
+class ModalView(BaseModel):
+    type: Type = Field(Type.MODAL, const=True)
+    title: PlainText
+    blocks: list[LayoutBlock]
+    close: PlainText = None
+    submit: PlainText = None
+    private_metadata: str = None
+    callback_id: str = None
+    clear_on_close: bool = False
+    notify_on_close: bool = False
+    external_id: str = None
+    submit_disabled: bool = False
+
+
+class HomeView(BaseModel):
+    type: Type = Field(Type.HOME, const=True)
+    blocks: list[LayoutBlock]
+    private_metadata: str = None
+    callback_id: str = None
+    external_id: str = None
